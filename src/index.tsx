30cdb4a676147803ff564e28b4002b92ee7eddae
storyroad <msknoel12@gmail.com>
2025-11-26 21:07:16 -0500

    Import CSS file in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-﻿import React from 'react';
+import './index.css';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { App } from './App';
