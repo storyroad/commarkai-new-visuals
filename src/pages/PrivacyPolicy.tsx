a3def230f38afe794697e04d6609d6e3b83da327
storyroad <msknoel12@gmail.com>
2025-11-26 21:38:52 -0500

    Fix copyright notice in PrivacyPolicy component

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -142,7 +142,7 @@ export default function PrivacyPolicy() {
       <footer className="border-t border-gray-200 bg-white/50 mt-16">
         <div className="container mx-auto px-6 py-8 text-center">
           <p className="text-gray-600 text-sm">
-            ┬⌐ {new Date().getFullYear()} CommarkAI. All rights reserved.
+            {new Date().getFullYear()} CommarkAI. All rights reserved.
           </p>
         </div>
       </footer>
